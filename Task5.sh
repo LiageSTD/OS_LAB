85d2347697c8071ce92338f15cb718315468ee3e
Ushenko_IAroslav <368952@niuitmo.ru>
2023-10-16 14:09:06 +0300

    Task5.sh

--- Task5.sh
+++ Task5.sh
@@ -1,45 +1,66 @@
 #!/bin/bash
 
+# Создаем временный файл для результатов
 tempfile=$(mktemp)
 
-declare -A parent_art_sum
+# Объявляем переменные для отслеживания текущего PPID и суммы ART
+current_ppid=""
+sum_art=0
+count=0
 
-declare -A parent_child_count
-
+# Цикл для обхода всех процессов в /proc
 for pid_dir in /proc/*/; do
     pid=$(basename "$pid_dir")
 
+    # Проверяем, является ли текущий элемент директорией и состоит ли его имя из цифр (PID)
     if [ -d "$pid_dir" ] && [[ "$pid" =~ ^[0-9]+$ ]]; then
+        # Получаем PPID из файла status
         ppid=$(grep -E '^PPid:' "$pid_dir/status" | awk '{print $2}')
 
+        # Получаем sum_exec_runtime из файла sched
         sum_exec_runtime=$(grep -E '^sum_exec_runtime' "$pid_dir/sched" | awk '{print $3}')
         
+        # Получаем nr_switches из файла sched
         nr_switches=$(grep -E '^nr_switches' "$pid_dir/sched" | awk '{print $3}')
 
+        # Вычисляем ART (Average Running Time)
         if [ -n "$sum_exec_runtime" ] && [ -n "$nr_switches" ] && [ "$nr_switches" -ne 0 ]; then
             ART=$(echo "scale=2; $sum_exec_runtime / $nr_switches" | bc)
         else
             ART=0.00
         fi
 
-        echo "ProcessID=$pid : Parent_ProcessID=$ppid : Average_Running_Time=$ART" >> "$tempfile"
+        # Если PPID изменился, добавляем строку "Average_Running_Children_of_ParentID=N is M"
+        if [ "$ppid" != "$current_ppid" ]; then
+            if [ -n "$current_ppid" ]; then
+                avg_art=$(echo "scale=2; $sum_art / $count" | bc)
+                echo "Average_Running_Children_of_ParentID=$current_ppid is $avg_art" >> "$tempfile"
+            fi
+            current_ppid="$ppid"
+            sum_art=0
+            count=0
+        fi
 
-        parent_art_sum["$ppid"]=$(echo "${parent_art_sum[$ppid]} + $ART" | bc)
-        parent_child_count["$ppid"]=$((parent_child_count["$ppid"] + 1))
+        # Добавляем информацию во временный файл
+        echo "ProcessID=$pid : Parent_ProcessID=$ppid : Average_Running_Time=$ART" >> "$tempfile"
+        
+        # Обновляем сумму ART и счетчик
+        sum_art=$(echo "scale=2; $sum_art + $ART" | bc)
+        count=$((count + 1))
     fi
 done
 
+# Добавляем последнюю группу, если она существует
+if [ -n "$current_ppid" ]; then
+    avg_art=$(echo "scale=2; $sum_art / $count" | bc)
+    echo "Average_Running_Children_of_ParentID=$current_ppid is $avg_art" >> "$tempfile"
+fi
+
+# Сортируем строки по идентификаторам родительских процессов
 sort -t '=' -k5 -n "$tempfile" > sorted_results.txt
 
-while IFS= read -r line; do
-    ppid=$(echo "$line" | awk -F'=' '{print $4}')
-    if [ -n "${parent_child_count[$ppid]}" ]; then
-        avg_art=$(echo "scale=2; ${parent_art_sum[$ppid]} / ${parent_child_count[$ppid]}" | bc)
-        echo "Average_Running_Children_of_ParentID=$ppid is $avg_art" >> sorted_results_with_avg.txt
-    fi
-    echo "$line" >> sorted_results_with_avg.txt
-done < sorted_results.txt
+# Выводим результат в консоль
+cat sorted_results.txt
 
-cat sorted_results_with_avg.txt
-
-rm "$tempfile" sorted_results.txt sorted_results_with_avg.txt
+# Удаляем временный файл
+rm "$tempfile"
