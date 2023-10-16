1bcc3281f76a2639b9b03969da33acc88506ae8a
Ushenko_IAroslav <368952@niuitmo.ru>
2023-10-16 16:13:56 +0300

    Task7.sh

--- Task7.sh
+++ Task7.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-iotop -b -n 1 -o > iotop_output.txt
+pidstat 1 60 -d | grep -v '^Linux' | grep -v '^# ' | awk '$2 ~ /^[0-9]/' > pidstat_output.txt
 
-processes=$(grep -E '^  \d+' iotop_output.txt | head -n 3)
+processes=$(cat pidstat_output.txt | tail -n +3 | sort -k6 -n -r | head -n 3)
 
 while read -r line; do
     pid=$(echo "$line" | awk '{print $1}')
-    cmd=$(echo "$line" | awk '{$1=""; print $0}')
-    io_data=$(echo "$line" | awk '{print $4}')
+    cmd=$(echo "$line" | awk '{$1=""; $2=""; print $0}')
+    io_data=$(echo "$line" | awk '{print $6}')
 
     echo "PID=$pid : Command=$cmd : Data_Read=$io_data"
 done <<< "$processes"
 
-rm iotop_output.txt
+rm pidstat_output.txt
